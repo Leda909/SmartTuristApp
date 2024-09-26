90ce9ebe723e5d0c5d8b0c058c2b27d9aa6743b1
Leda909 <ko9zad@gmail.com>
2024-09-26 19:57:48 +0100

    remove committed

--- public/js/map.js
+++ public/js/map.js
@@ -38,34 +38,6 @@ async function createMap(googleApiKey) {
     });
 }
 
-// async function createMap() {
-//     const googleApiKey = await fetchGoogleApiKey();
-
-//     (g => {
-//         var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window;
-//         b = b[c] || (b[c] = {});
-//         var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams,
-//             u = () => h || (h = new Promise(async (f, n) => {
-//                 await (a = m.createElement("script"));
-//                 e.set("libraries", [...r] + "");
-//                 for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
-//                 e.set("callback", c + ".maps." + q);
-//                 a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
-//                 d[q] = f;
-//                 a.onerror = () => h = n(Error(p + " could not load."));
-//                 a.nonce = m.querySelector("script[nonce]")?.nonce || "";
-//                 m.head.append(a);
-//             }));
-//         d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
-//     })({
-        
-//         key: googleApiKey,
-//         v: "weekly",
-//     });
-
-//     let map;
-// }
-
 // Function to orchestrate the loading of the map
 async function init() {
     try {
@@ -79,11 +51,6 @@ async function init() {
   
   // Call the init function to start the process
 init();
-
-// First create google map before add markers.
-// createMap().then(()=>{
-//     initMap();
-// })
 
 
 
